0c7bed3992c9c543ed09799e9da7b246673d246d
Eferdez <81372770+Eferdez@users.noreply.github.com>
2024-07-31 08:42:20 -0300

    Update Counter3.sol

--- Counter3.sol
+++ Counter3.sol
@@ -1,60 +1,59 @@
 // SPDX-License-Identifier: MIT
+
 pragma solidity ^0.8.3;
 
 contract Counter3 {
-   uint anotherNumber;
-   bool paused;
-   address owner;
-   mapping (address=> uint) counts;
+    uint anotherNumber;
+    bool paused;
+    address owner;
+    mapping (address => uint) counts;
 
-   error NotOwner();
-   error NotOne();
-   error ContractPaused();
+    error NotOwner();
+    error NotOne();
+    error ContractPaused();
 
-   constructor() {
-       owner = msg.sender;
-   }
+    constructor() {
+        owner = msg.sender;
+    }
 
-     modifier onlyOwner(){
-        if (msg.sender != owner){
+    modifier onlyOwner() {
+        if (msg.sender != owner) {
             revert NotOwner();
         }
         _;
-     }
-     
-     modifier notOne(){
-        if (anotherNumber != 1){
-            revert NotOne() ;
+    }
+
+    modifier notOne() {
+        if (counts[owner] != 1) {
+            revert NotOne();
         }
         _;
-        }
-        
+    }
 
-        modifier notPaused(){
-            if (paused == true){
-                revert ContractPaused();
-            }
-            _;
+    modifier notPaused() {
+        if (paused == true) {
+            revert ContractPaused();
         }
+        _;
+    }
 
-    function pause() public onlyOwner notPaused{
+    function pause() public onlyOwner notPaused {
         paused = true;
     }
 
-     function unpause() public onlyOwner{
+    function unpause() public onlyOwner {
         paused = false;
-    }     
+    }
+
 
     function getCount() public view notOne returns (uint) {
         return counts[msg.sender];
     }
 
-   
     function inc() public {
         counts[msg.sender] += 1;
     }
 
-  
     function dec() public {
         counts[msg.sender] -= 1;
     }
